0342b8dcb13d7fef521e40c2d4c1f0f01170d497
pacicco <102540905+pacicco@users.noreply.github.com>
2022-06-14 17:38:48 -0300

    pesquisa de usuários concluída

---
+++ pages/api/pesquisa.ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
+import { validarTokenJWT } from '../../middlewares/validarTokenJWT';
+import { UsuarioModel } from '../../models/usuarioModels';
+import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg'
+
+const PesquisaEndpoint
+    = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg | any>) => {
+        try {
+            if (req.method === 'GET') {
+                const { filtro } = req.query;
+                if (!filtro || filtro.length < 2) {
+                    return res.status(400).json({ erro: 'Favor informar pelo menos 2 caracteres' });
+                }
+                const usuariosEncontrados = await UsuarioModel.find({
+                    $or: [{ nome: {$regex : filtro, $options : 'i'}},
+                           {email: {$regex: filtro, $options: 'i' }}] 
+                    
+                });
+                return res.status (200).json (usuariosEncontrados); 
+            }
+            return res.status (405).json ({erro: 'Metodo informado nao e valido'});
+        } catch (e) {
+            console.log(e);
+            return res.status(500).json({ erro: 'Nao foi possivel buscar o usuario' })
+
+        }
+    }
+
+
+export default validarTokenJWT(conectarMongoDB(PesquisaEndpoint));    
