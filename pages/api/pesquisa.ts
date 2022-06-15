40f2e932c76d986f0c15f51f89571986b27386ef
pacicco <102540905+pacicco@users.noreply.github.com>
2022-06-15 12:35:18 -0300

    conclusão da home de usuário especifico

--- pages/api/pesquisa.ts
+++ pages/api/pesquisa.ts
@@ -8,18 +8,32 @@ const PesquisaEndpoint
     = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg | any>) => {
         try {
             if (req.method === 'GET') {
+                if (req?.query?.id) {
+                    const usuariosEncontrados = await UsuarioModel.findById(req?.query?.id);
+                    if (!usuariosEncontrados) {
+                        return res.status(400).json({ erro: 'Usuario nao encontrado' });
+                    }
+                    usuariosEncontrados.senha = null;
+                    return res.status(200).json(usuariosEncontrados);
+
+                }
+
+            } else {
                 const { filtro } = req.query;
                 if (!filtro || filtro.length < 2) {
                     return res.status(400).json({ erro: 'Favor informar pelo menos 2 caracteres' });
                 }
-                const usuariosEncontrados = await UsuarioModel.find({
-                    $or: [{ nome: {$regex : filtro, $options : 'i'}},
-                           {email: {$regex: filtro, $options: 'i' }}] 
-                    
-                });
-                return res.status (200).json (usuariosEncontrados); 
+
+                const usuariosEncontrados = await UsuarioModel.find ({
+
+                    $or: [{ nome: { $regex: filtro, $options: 'i' } },
+                    { email: { $regex: filtro, $options: 'i' } }]
+
+                });    
+
+
             }
-            return res.status (405).json ({erro: 'Metodo informado nao e valido'});
+            return res.status(405).json({ erro: 'Metodo informado nao e valido' });
         } catch (e) {
             console.log(e);
             return res.status(500).json({ erro: 'Nao foi possivel buscar o usuario' })
