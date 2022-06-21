3b6a036da6e8cf445f805d989334214ed2775a00
pacicco <102540905+pacicco@users.noreply.github.com>
2022-06-21 12:27:01 -0300

    conclusão api feed,publi e comentarios

---
+++ pages/api/comentario.ts
@@ -0,0 +1,45 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { conectarMongoDB } from "../../middlewares/conectarMongoDB";
+import { validarTokenJWT } from "../../middlewares/validarTokenJWT";
+import { PublicacaoModel } from "../../models/PublicacacoModel";
+import { UsuarioModel } from "../../models/usuarioModels";
+import type { RespostaPadraoMsg } from "../../types/RespostaPadraoMsg";
+
+const comentarioEndpoint = async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+        if (req.method === 'PUT'){
+            const {userId, id} = req.query;
+            const usuarioLogado = await UsuarioModel.findById(userId);
+            if (!usuarioLogado){
+                return res.status (400).json ({erro: 'Usuario nao encontrado'})
+            }
+
+            const publicacao = await PublicacaoModel.findById (id);
+            if (!publicacao){
+                return res.status (400).json ({erro: 'Publicacao nao encontrada'});
+            }
+
+            if (!req.body || !req.body.comentario
+                || req.body.comentario.length < 2){
+                    return res.status (400).json ({erro: 'Comentario nao e valido'});
+                }
+
+            const comentario = {
+                usuarioId : usuarioLogado._id,
+                nome : usuarioLogado.nome,
+                comentario : req.body.comentario
+            }    
+
+            publicacao.comentarios.push (comentario);
+            await PublicacaoModel.findByIdAndUpdate ({_id: publicacao})
+            return res.status (200).json ({msg: 'comentario adicionado com sucesso'})
+        }
+
+        return res.status (405).json ({erro: 'Metodo informado nao e valido'});
+    }catch(e){
+        console.log (e);
+        return res.status (500).json ({erro: 'Ocorreu ao adicionar comentarios'});
+    }
+}
+
+export default validarTokenJWT (conectarMongoDB(comentarioEndpoint));
--- pages/api/feed.ts
+++ pages/api/feed.ts
@@ -4,6 +4,7 @@ import { validarTokenJWT } from "../../middlewares/validarTokenJWT";
 import { conectarMongoDB } from "../../middlewares/conectarMongoDB";
 import { UsuarioModel } from "../../models/usuarioModels";
 import { PublicacaoModel } from "../../models/PublicacacoModel";
+import { SeguidorModel } from "../../models/SeguidorModel";
 
 const feedEndpoint = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg | any>) => {
     try {
@@ -19,9 +20,44 @@ const feedEndpoint = async (req: NextApiRequest, res: NextApiResponse<RespostaPa
                     .sort({ data: -1 });
 
                 return res.status(200).json(publicacoes);
-            }
+            } else {
+                const { userId } = req.query;
+                const usuarioLogado = await UsuarioModel.findById(userId);
+                if (!usuarioLogado) {
+                    return res.status(400).json({ erro: 'usuario nao encontrado' });
+                }
 
+                const seguidores = await SeguidorModel.find({ usuarioId: usuarioLogado._id });
+                const seguidoresIds = seguidores.map(s => s.usuarioASerSeguidoId);
+
+                const publicacoes = await PublicacaoModel.find({
+                    $or: [
+                        { idUsuario: usuarioLogado._id },
+                        { idUsuario: seguidoresIds }
+
+                    ]
+                })
+                    .sort({ data: -1 });
+
+
+                const result = [];
+                for (const publicacao of publicacoes) {
+                    const usuarioDaPublicacao = await UsuarioModel.findById(publicacao.idUsuario);
+                    if (usuarioDaPublicacao) {
+                        const final { ...publicacao._doc, usuario: {
+                            nome : usuarioDaPublicacao.nome,
+                            avatar : usuarioDaPublicacao.avatar
+
+                        } };
+                        result.push (final);
+                    }
+
+                }
+                return res.status(200).json(publicacoes)
+            }
         }
+
+
         return res.status(405).json({ erro: 'Metodo informado nao e valido' });
     } catch (e) {
         console.log(e);
